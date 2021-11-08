f1767316e111a0aedaef630ea54b20ed744294a0
Salvydas Lukosius <sall@w-ss.io>
2021-11-08 14:04:10 +0000

    new file:   .pre-commit-config.yaml 	new file:   LICENSE 	new file:   README.md 	new file:   layouts/contract.cr-layout 	new file:   layouts/default.cr-layout 	new file:   themes/blue-256.cr-theme 	new file:   themes/clean-256.cr-theme 	new file:   themes/clean-256.cr-theme.zwc 	new file:   themes/default.cr-theme 	new file:   themes/default.cr-theme.zwc 	new file:   themes/forest-256.cr-theme 	new file:   themes/free-256.cr-theme 	new file:   themes/safari-256.cr-theme 	new file:   themes/zdharma-256.cr-theme 	new file:   themes/zdharma.cr-theme 	new file:   ziconsole 	new file:   ziconsole.conf 	new file:   zinit-console.plugin.zsh 	new file:   zpconsole 	new file:   zpconsole.conf

---
+++ zinit-console.plugin.zsh
@@ -0,0 +1,38 @@
+#
+# No plugin manager is needed to use this file. All that is needed is adding:
+#   source {where-zplugin-view-is}/zplugin-view.plugin.zsh
+#
+# to ~/.zshrc. But of course, this plugin is an extension to Zplugin.
+#
+
+# Standarized $0 handling
+# http://z-shell.github.io/zinit/wiki/zsh-plugin-standard/
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+typeset -gA Plugins
+Plugins[ZICONSOLE_REPO_DIR]="${0:h}"
+
+if [[ "${+functions[-zui_std_cleanup]}" = "0" ]]; then
+    echo "The zplugin consolette uses ZUI plugin, please load https://github.com/z-shell/zui/ with your plugin manager, or source it."
+fi
+
+#
+# Update FPATH if:
+# 1. Not loading with Zplugin or other plugin manager supporting the plugin standard
+# 2. Not having fpath already updated (that would equal: using other plugin manager)
+#
+
+if [[ $zsh_loaded_plugins[-1] != */zplugin-console && -z $fpath[(r)${0:h}] ]]
+then
+    fpath+=( "${0:h}" )
+fi
+
+[[ -z "${fg_bold[green]}" ]] && builtin autoload -Uz colors && colors
+
+autoload -- ziconsole zpconsole
+
+zle -N ziconsole
+bindkey "^O^J" ziconsole
+
+# vim:ft=zsh
